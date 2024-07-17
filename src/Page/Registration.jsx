3b0d29db0f09d79004108918484d237a7f397167
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-07-17 18:52:17 +0600

    add: handleRegistration

--- src/Page/Registration.jsx
+++ src/Page/Registration.jsx
@@ -1,37 +1,87 @@
+
 const Registration = () => {
+
+  const handleRegistration = (e)=>{
+    e.preventDefault();
+    const userInfo = {
+      user_name: e.target.name.value,
+      user_phone: e.target.phone.value,
+      user_email: e.target.email.value,
+      user_pin: e.target.pin.value,
+    }
+    console.log(userInfo);
+  }
   return (
     <>
-      <div className="hero bg-gradient-to-r from-[#87CEEB] to-[#ADD8E6] min-h-screen">       
-        <div className="hero-content flex-col lg:flex-row-reverse">
+      <div className="bg-gradient-to-r from-[#87CEEB] to-[#ADD8E6] min-h-screen flex items-center">
+        <div className="hero-content gap-10 mx-auto">
           <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Register now!</h1>
+            <h1 className="text-5xl font-bold">
+              Welcome to <span className="text-blue-600">EasyPay</span>!
+            </h1>
             <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+              Experience seamless transactions with just a few clicks. Sign up
+              now!
             </p>
           </div>
-          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <form className="card-body">
+          <div className="card bg-base-100 w-full max-w-md shrink-0 shadow-2xl">
+            <form onSubmit={handleRegistration} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="your name"
+                  name="name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Mobile Number</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="your phone number"
+                  className="input input-bordered"
+                  name="phone"
+                  onInput={(e) =>
+                    (e.target.value = e.target.value.replace(/[^0-9]/g, ""))
+                  }
+                  maxLength={11}
+                  required
+                />
+              </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
                 <input
                   type="email"
-                  placeholder="email"
+                  placeholder="your email"
+                  name="email"
                   className="input input-bordered"
                   required
                 />
               </div>
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text">Password</span>
+                  <span className="label-text">PIN</span>
                 </label>
                 <input
                   type="password"
-                  placeholder="password"
+                  inputMode="numeric"
+                  pattern="[0-9]*"
+                  maxLength={5}
+                  onInput={(e) =>
+                    (e.target.value = e.target.value.replace(/[^0-9]/g, ""))
+                  }
+                  placeholder="set a 5-digit pin"
                   className="input input-bordered"
+                  name="pin"
                   required
                 />
                 <label className="label">
