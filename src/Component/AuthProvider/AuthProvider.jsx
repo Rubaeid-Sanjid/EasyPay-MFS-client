063de7a739dd959b517c316340d106eaf9986f32
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-08-03 17:29:56 +0600

    add:on auth change

--- src/Component/AuthProvider/AuthProvider.jsx
+++ src/Component/AuthProvider/AuthProvider.jsx
@@ -1,24 +1,42 @@
 import PropTypes from 'prop-types';
-import { createContext } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import { auth } from '../Firebase/FirebaseConfig';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, updateProfile } from 'firebase/auth';
 
 export const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     const createUser = (email, pin)=>{
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, pin)
     }
     
     const updateUser = (userName)=>{
+        setLoading(true);
         return updateProfile(auth.currentUser, {displayName: userName })
     }
 
     const loginUser = async(email, pin)=>{
+        setLoading(true);
         return await signInWithEmailAndPassword(auth, email, pin)
     }
 
+    useEffect(()=>{
+        const unSubscribe = onAuthStateChanged(auth, (currentUser)=>{
+            if(currentUser){
+                setUser(currentUser)
+                
+            }
+            setLoading(false)
+        })
+
+        return ()=>{
+            unSubscribe();
+        }
+    },[])
     // window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
     //     'size': 'invisible',
     //     'callback': () => {
@@ -30,7 +48,7 @@ const AuthProvider = ({children}) => {
     //     return signInWithPhoneNumber(auth, phoneNumber, window.recaptchaVerifier)
     // }
 
-    const authInfo = {createUser, updateUser, loginUser};
+    const authInfo = {user, loading, createUser, updateUser, loginUser};
 
     return (
         <AuthContext.Provider value={authInfo}>
