b9b560157003db7c8ef6f7e3518dc72e023a08f0
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-08-10 20:22:02 +0600

    add:response interceptor

--- src/Component/Hooks/useAxiosPrivate.jsx
+++ src/Component/Hooks/useAxiosPrivate.jsx
@@ -1,13 +1,21 @@
 import axios from "axios";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
 
 export const axiosSecure = axios.create({
   baseURL: "http://localhost:5000",
 });
 
 const useAxiosPrivate = () => {
+  const { logOutUser } = useAuth();
+  const navigate = useNavigate();
+
   axiosSecure.interceptors.request.use(
     function (config) {
       // Do something before request is sent
+      const token = localStorage.getItem("token");
+
+      config.headers.authorization = `Bearer ${token}`;
       return config;
     },
     function (error) {
@@ -19,9 +27,15 @@ const useAxiosPrivate = () => {
     function (response) {
       return response;
     },
-    function (error) {
+    async (error) => {
       // Any status codes that falls outside the range of 2xx cause this function to trigger
       // Do something with response error
+      const status = error.response.status;
+
+      if (status === 401 || status === 403) {
+        await logOutUser();
+        navigate("/");
+      }
       return Promise.reject(error);
     }
   );
